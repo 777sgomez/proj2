83b6c120ef1fdab25b1b9c93c70c14042661574b
Susan Gomez <susangomez@Susans-MBP.attlocal.net>
2023-07-08 18:09:40 -0400

    Initial commit

---
+++ project2.js
@@ -0,0 +1,68 @@
+let userScores = 0;
+let computerScores = 0;
+let tiesScore = 0;
+
+let computerChoice;
+let userChoice;
+
+const userChoiceW = document.getElementById("userChoiceW");
+const gameTies = document.getElementById("gameTies");
+const computerChoiceL = document.getElementById("computerChoiceL");
+
+const buttonRock = document.getElementById("buttonRock");
+const buttonPaper = document.getElementById("buttonPaper");
+const buttonScissors = document.getElementById("buttonScissors");
+const buttonChoices = document.querySelectorAll("button");
+
+
+buttonChoices.forEach(buttonChoice => buttonChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id
+    userChoiceW.innerHTML = userChoice
+    generateComputerChoice()
+}))
+
+function generateComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    if (randomNumber === 0) {
+        computerChoice = "Rock"
+    }
+    if (randomNumber === 1) {
+        computerChoice = "Paper"
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Scissors"
+    }
+    computerChoiceL.innerHTML = computerChoice
+}
+
+function playRound()
+{
+    if (userChoice === computerChoice)
+    {
+        tiesScore = tiesScore++; tiesScore++; gameTies.textContent = tiesScore;
+    }
+    else if (userChoice === "Rock")
+    {
+        if(computerChoice ==="Scissors")
+        {
+            userScores = userScores++; userScores++; userChoiceW.textContent = userScores;
+        }
+    }
+    else if (computerChoice === "Paper")
+    {
+        computerScores = computerScores++; computerScores++; computerChoiceL.textContent = computerScores;
+    }
+    else if (userChoice === "Scissors")
+    {
+        if(computerChoice === "Rock")
+        {
+            userScores = userScores++; userScores++; userChoiceW.textContent = userScores;
+        }
+    }
+    else if(computerChoice === "Paper")
+    {
+        userScores = userScores++; userScores++; userChoiceW.textContent = userScores;
+    }
+    return (userChoice, computerChoice)
+} 
